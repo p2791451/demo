8634c3c82b4ee3895defe1bcd29dc7e9c31b7956
Prasad Hegde <c-prasad.hegde@charter.com>
2018-09-08 21:56:16 +0530

    1TC

--- my_python.py
+++ my_python.py
@@ -1,6 +1,7 @@
 import sys
 
 for arg in sys.argv:
-    if (arg) == "module":
-        print("Module: ", arg)
+    if (arg) in ['All', 'Dvr', 'cGuide', 'Vod', 'Search']:
+        print(arg)
+        print("Module is : ", arg)
 
